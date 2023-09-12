ab6e409938eb245d4c943dbe4331c77a1928438b
Snaehath <2020ad0556@svce.ac.in>
2023-09-12 22:23:17 +0530

    fixed typo

--- src/Components/Dish.tsx
+++ src/Components/Dish.tsx
@@ -3,7 +3,7 @@ export default function Dish() {
         <main>
             <h1>Fun Facts about React</h1>
             <ul>
-                <li>is published facebook</li>
+                <li>is published by facebook</li>
                 <li>uses small components</li>
                 <li>Virtual DOM is better than Real DOM</li>
                 <li>is not a framework</li>
